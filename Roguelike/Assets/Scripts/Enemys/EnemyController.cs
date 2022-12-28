fdd7dd5b98abd683f433f406e630b48697a0808c
ole.brenner <ole.brenner@stud.uni-hannover.de>
2022-12-28 20:08:32 +0100

    Added enemy attack based on raycast

--- Roguelike/Assets/Scripts/Enemys/EnemyController.cs
+++ Roguelike/Assets/Scripts/Enemys/EnemyController.cs
@@ -28,29 +28,62 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        animator.ResetTrigger("attackAnimation");
-
-        RaycastHit hit;
-        Debug.DrawRay(transform.position, new Vector3(35, 0, 0), Color.yellow);
-        Debug.DrawRay(transform.position, new Vector3(-35, 0, 0), Color.yellow);
-        Debug.DrawRay(transform.position, new Vector3(0, 35, 0), Color.green);
-        Debug.DrawRay(transform.position, new Vector3(0, -35, 0), Color.green);
-        // if (Physics.Raycast(transform.position, new Vector3(1, 0, 0), out hit, layerMask))
-        //  { 
-        //     Debug.DrawRay(transform.position, new Vector3(1, 0, 0), Color.yellow);
-        //  }
-
-        if (Input.GetKeyDown("space"))
-        {
-            animator.SetTrigger("attackAnimation");
-        }
-
         float horizontalMovement = Input.GetAxis("Horizontal");
         float verticalMovement = Input.GetAxis("Vertical");
 
-
         if (Mathf.Log(Mathf.Abs(verticalMovement), 3) >= Mathf.Log(Mathf.Abs(horizontalMovement), 3)) horizontalMovement = 0;
         else verticalMovement = 0;
+
+
+        animator.ResetTrigger("attackAnimation");
+        LayerMask mask = LayerMask.GetMask("Player_Hitbox");
+
+        Debug.DrawRay(transform.position, new Vector3(35.0f, 0, 0), Color.yellow);
+        Debug.DrawRay(transform.position, new Vector3(-35.0f, 0, 0), Color.yellow);
+        Debug.DrawRay(transform.position, new Vector3(0, 35.0f, 0), Color.yellow);
+        Debug.DrawRay(transform.position, new Vector3(0, -35.0f, 0), Color.yellow);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(1, 0), 35.0f, mask.value);
+        if (hit.collider != null)
+        {
+            Debug.Log("Ray hit: " + hit.collider.gameObject + " at " + hit.point);
+
+            animator.SetFloat("yDirection", 0);
+            animator.SetFloat("xDirection", 1);
+
+            animator.SetTrigger("attackAnimation");
+        }
+         hit = Physics2D.Raycast(transform.position, new Vector2(-1, 0), 35.0f, mask.value);
+        if (hit.collider != null)
+        {
+            Debug.Log("Ray hit: " + hit.collider.gameObject + " at " + hit.point);
+
+            animator.SetFloat("yDirection", 0);
+            animator.SetFloat("xDirection", -1);
+
+            animator.SetTrigger("attackAnimation");
+        }
+        hit = Physics2D.Raycast(transform.position, new Vector2(0, 1), 35.0f, mask.value);
+        if (hit.collider != null)
+        {
+            Debug.Log("Ray hit: " + hit.collider.gameObject + " at " + hit.point);
+
+            animator.SetFloat("yDirection", 1);
+            animator.SetFloat("xDirection", 0);
+
+            animator.SetTrigger("attackAnimation");
+        }
+         hit = Physics2D.Raycast(transform.position, new Vector2(0, -1), 35.0f, mask.value);
+        if (hit.collider != null)
+        {
+            Debug.Log("Ray hit: " + hit.collider.gameObject + " at " + hit.point);
+
+            animator.SetFloat("yDirection", -1);
+            animator.SetFloat("xDirection", 0);
+
+            animator.SetTrigger("attackAnimation");
+        }
+
 
 
 
@@ -76,6 +109,24 @@ public class EnemyController : MonoBehaviour
         this.movementSpeed = movementSpeed;
         animator.SetFloat("movementAnimationSpeed", this.movementSpeed / 5.0f);
     }
+
+    void FixedUpdate()
+    {
+        LayerMask mask = LayerMask.GetMask("Player_Hitbox");
+        //RaycastHit hit;
+        Debug.DrawRay(transform.position, new Vector3(35.0f, 0, 0), Color.yellow);
+        //Debug.DrawRay(transform.position, new Vector3(-35, 0, 0), Color.yellow);
+        //Debug.DrawRay(transform.position, new Vector3(0, 35, 0), Color.green);
+        //Debug.DrawRay(transform.position, new Vector3(0, -35, 0), Color.green);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(1,0),35.0f,mask.value);
+        if (hit.collider != null)
+        {
+            Debug.Log("Ray hit: " + hit.collider.gameObject + " at " + hit.point);
+            animator.SetTrigger("attackAnimation");
+        }
+    }
+
+
 
     private void shoot()
     {
